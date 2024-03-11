a7cc881b4001ffba5c714ebb37df9f0cd78ec414
dinara-khusainova <xusainova.dinara@bk.ru>
2024-03-11 14:49:19 +0300

    report and presentation lab 6

---
+++ labs/lab6/lab6_2.jl
@@ -0,0 +1,51 @@
+using Plots
+using DifferentialEquations
+
+N = 8439
+I0 = 86
+R0 = 25
+S0 = N - I0 - R0 
+alpha = 0.4 
+beta = 0.1 
+
+function func(du, u, p, t)
+    S, I, R = u
+    du[1] = -alpha*u[1]
+    du[2] = alpha*u[1] - beta*u[2]
+    du[3] = beta*I
+end
+
+v0 = [S0, I0, R0]
+tspan = (0.0, 120.0)
+prob = ODEProblem(func, v0, tspan)
+sol = solve(prob, dtmax=0.05)
+S = [u[1] for u in sol.u]
+I = [u[2] for u in sol.u]
+R = [u[3] for u in sol.u]
+T = [t for t in sol.t]
+
+plt = plot(
+  dpi=600,
+  legend=:right)
+
+plot!(
+  plt,
+  T,
+  S,
+  label="Восприимчивые особи",
+  color=:blue)
+plot!(
+  plt,
+  T,
+  I,
+  label="Инфицированные особи",
+  color=:red)
+plot!(
+  plt,
+  T,
+  R,
+  label="Особи с иммунитетом",
+  color=:green)
+
+
+savefig(plt, "lab6_2.png")
